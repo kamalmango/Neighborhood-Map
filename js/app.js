c36011ca8a5807392ff2ff594d11103006b77c63
Kamal Mango <kamal.mango7@gmail.com>
2015-08-18 15:02:07 -0400

    start

---
+++ js/app.js
@@ -0,0 +1,107 @@
+
+var map;
+var infowindow;
+
+function initMap() {
+  var latlng = {lat: 38.9047, lng: -77.0164};
+
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: latlng,
+    zoom: 14
+  });
+
+  infowindow = new google.maps.InfoWindow();
+
+  var service = new google.maps.places.PlacesService(map);
+  service.nearbySearch({
+    location: latlng,
+    radius: 2000, //500,
+    types: ['restaurant']
+  }, callback);
+}
+
+function callback(results, status) {
+  if (status === google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      createMarker(results[i]);
+    }
+  }
+}
+
+function createMarker(place) {
+  var placeLoc = place.geometry.location;
+  var marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location
+  });
+
+  google.maps.event.addListener(marker, 'click', function() {
+    infowindow.setContent(place.name);
+    infowindow.open(map, this);
+  });
+}
+
+initMap();
+
+
+
+/*
+var markers = [
+        ['Georgetown Waterfront Park', 38.9022, -77.0619],
+        ['The White House', 38.8977,  -77.0366],
+        ['Verizon Center', 38.8981, -77.0208]
+    ]
+
+
+function initMap() {
+    var latlng = new google.maps.LatLng(38.9047, -77.0164);
+    var myOptions = {
+        zoom: 14,
+        center: latlng,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        mapTypeControl: false
+    };
+    var map = new google.maps.Map(document.getElementById("map"),myOptions);
+
+    var infowindow = new google.maps.InfoWindow(), marker, i;
+
+    for (i = 0; i < markers.length; i++) {  
+        marker = new google.maps.Marker({
+            position: new google.maps.LatLng(markers[i][1], markers[i][2]),
+            map: map
+        });
+        google.maps.event.addListener(marker, 'click', (function(marker, i) {
+            return function() {
+                infowindow.setContent(markers[i][0]);
+                infowindow.open(map, marker);
+            }
+        })(marker, i));
+    }
+}
+
+initMap();
+*/
+
+
+var ViewModel = function() {
+	 
+};
+
+ko.applyBindings(new ViewModel());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
